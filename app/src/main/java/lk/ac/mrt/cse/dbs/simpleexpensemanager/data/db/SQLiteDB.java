40690b337559c14d329cb3d07a41183985057b0a
Oshadi20 <120305923+Oshadi20@users.noreply.github.com>
2022-12-13 00:56:35 +0530

    my second commit

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/db/SQLiteDB.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/db/SQLiteDB.java
@@ -1,2 +1,145 @@
-package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.db;public class SQLiteDB {
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+import android.database.Cursor;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+
+public class SQLiteDB extends SQLiteOpenHelper {
+    private static final String DATABASE = "200458M.db";
+
+    private static final String ACCOUNT_TB = "account";
+
+    private static final String TRANSACTION_TB = "transactions";
+
+    private static final int DEFAULT_LIMIT = 0;
+
+    public SQLiteDB(@Nullable Context context) {
+        super(context, DATABASE, null, 2);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE "+ACCOUNT_TB+
+                " (accountno TEXT PRIMARY KEY ,"+
+                "bankname TEXT  ,"+
+                "accountHolderName TEXT, "+
+                "balance REAL"
+                +")");
+        sqLiteDatabase.execSQL("CREATE TABLE "+TRANSACTION_TB+
+                " (transaction_no INTEGER  PRIMARY KEY AUTOINCREMENT,"+
+                "accountno TEXT  ,"+
+                "date TEXT, "+
+                "expenseType TEXT ,"+
+                "amount REAL"
+                +")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ACCOUNT_TB);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TRANSACTION_TB);
+        onCreate(sqLiteDatabase);
+    }
+
+
+    public boolean insertData(String table_name,ContentValues content){
+        SQLiteDatabase db = this.getWritableDatabase();
+        long result;
+        try{
+            result = db.insertOrThrow(table_name, null,content);
+        }catch(Exception e){
+            result = -1;
+            System.out.println("Data Insert Error");
+        }
+
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+
+
+    public Cursor getDataWithLimit(String table_name, String [] columns, String [][] conditions,int limit){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String cols = "";
+        if (columns.length != 0){
+            for (int i = 0;i < columns.length ;i++){
+                cols += columns[i]+" , ";
+            }
+            cols = cols.substring(0,cols.length()-2);
+        }
+        String condition = "";
+        String[] args = null;
+        if(conditions.length != 0){
+            args = new String[conditions.length];
+            condition += " WHERE ";
+            for (int i = 0;i < conditions.length ;i++){
+                if(conditions[i].length == 3){
+                    String[] temp = conditions[i];
+                    condition += temp[0] + " "+temp[1]+" ? AND ";
+                    args[i] = temp[2];
+                }
+
+            }
+            condition = condition.substring(0,condition.length()-4);
+        }else{
+            condition = "";
+        }
+        String lim = "";
+        if(limit != 0){
+            lim = " LIMIT "+String.valueOf(limit);
+        }
+
+        String sql = "select "+cols+" from "+table_name+condition+lim;
+        Cursor result = db.rawQuery(sql,args);
+        return result;
+    }
+
+    public Cursor getData(String table_name, String [] columns, String [][] conditions){
+        return getDataWithLimit(table_name, columns, conditions,DEFAULT_LIMIT);
+    }
+
+    public boolean updateData(String table_name,ContentValues content, String[ ] condition){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String cond = condition[0]+" "+condition[1]+" ? ";
+        String[] args = {condition[2]};
+
+        long result;
+        try{
+            result = db.update(table_name, content,cond,args);
+        }catch (Exception e){
+
+            result = -1;
+        }
+
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public Integer deleteData(String table_name, String column, String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(table_name, column+" = ?", new String[] {id});
+    }
+
+    public void deleteTableContent(String table_name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("delete from "+table_name);
+    }
+
+
+
+
+
 }
--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
@@ -1,2 +1,131 @@
-package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;public class PersistentTransactionDAO {
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.PersistentExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.db.SQLiteDB;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
+
+public class PersistentTransactionDAO implements TransactionDAO {
+    private SQLiteDB myDataBase;
+
+    private static final String TRANSACTION_TB = "transactions";
+    private static final String TRANSACTION_DATE = "date";
+    private static final String TRANSACTION_ACCOUNTNO = "accountno";
+    private static final String TRANSACTION_EXPENSETYPE = "expenseType";
+    private static final String TRANSACTION_AMOUNT = "amount";
+
+    public PersistentTransactionDAO(SQLiteDB db){
+        this.myDataBase = db;
+    }
+
+    @Override
+    public void logTransaction(Date date, String accountNo, ExpenseType expenseType, double amount) {
+        if(expenseType == ExpenseType.EXPENSE){
+            PersistentAccountDAO pa = new PersistentAccountDAO(this.myDataBase);
+            try {
+                Account user = pa.getAccount(accountNo);
+                if(user.getBalance() < amount){
+                    return;
+                }
+            }catch (Exception e){
+                System.out.println("Invalid Account");
+            }
+        }
+        String sDate = date.toString();
+        ContentValues transContent = new ContentValues();
+        transContent.put(TRANSACTION_ACCOUNTNO, accountNo);
+        transContent.put(TRANSACTION_DATE, sDate);
+        transContent.put(TRANSACTION_EXPENSETYPE, getStringExpense(expenseType));
+        transContent.put(TRANSACTION_AMOUNT, amount);
+        this.myDataBase.insertData(TRANSACTION_TB, transContent);
+    }
+
+    @Override
+    public List<Transaction> getAllTransactionLogs() {
+        Cursor result = this.myDataBase.getData(TRANSACTION_TB,new String[] {"*"},new String[][] {});
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        if(result.getCount() != 0) {
+
+            while (result.moveToNext()) {
+                String dateS = result.getString(result.getColumnIndex(TRANSACTION_DATE));
+                String accountNo = result.getString(result.getColumnIndex(TRANSACTION_ACCOUNTNO));
+                String expenseType = result.getString(result.getColumnIndex(TRANSACTION_EXPENSETYPE));
+                double amount = result.getDouble(result.getColumnIndex(TRANSACTION_AMOUNT));
+                Date date = stringToDate(dateS);
+
+                Transaction transaction = new Transaction(date,accountNo,getExpense(expenseType),amount );
+                transactions.add(transaction);
+            }
+        }
+        result.close();
+        return transactions;
+    }
+
+    @Override
+    public List<Transaction> getPaginatedTransactionLogs(int limit) {
+        Cursor result = this.myDataBase.getDataWithLimit(TRANSACTION_TB,new String[] {"*"},new String[][] {},limit);
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        if(result.getCount() != 0) {
+
+            while (result.moveToNext()) {
+                String dateS = result.getString(result.getColumnIndex(TRANSACTION_DATE));
+                String accountNo = result.getString(result.getColumnIndex(TRANSACTION_ACCOUNTNO));
+                String expenseType = result.getString(result.getColumnIndex(TRANSACTION_EXPENSETYPE));
+                double amount = result.getDouble(result.getColumnIndex(TRANSACTION_AMOUNT));
+                Date date = stringToDate(dateS);
+                Transaction transaction = new Transaction(date,accountNo,getExpense(expenseType),amount );
+                transactions.add(transaction);
+            }
+        }
+        result.close();
+        return transactions;
+    }
+
+    public ExpenseType getExpense(String expense){
+        if(expense.equals("Expense")){
+            return ExpenseType.EXPENSE;
+        }else{
+            return ExpenseType.INCOME;
+        }
+    }
+
+    public String getStringExpense(ExpenseType expense){
+        if(expense == ExpenseType.EXPENSE){
+            return "Expense";
+        }else{
+            return "Income";
+        }
+    }
+
+    public Date stringToDate(String strDate){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);
+        Date date = new Date();
+        try{
+            date = dateFormat.parse(strDate);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return date;
+    }
+
+    public void removeTransaction(String transactionNo) throws InvalidAccountException {
+        int result = this.myDataBase.deleteData("transaction","transaction_no",transactionNo);
+        if(result == 0){
+            throw new InvalidAccountException("Transactoin number is invalid");
+        }
+    }
+
 }
